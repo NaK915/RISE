80c78b31445fbcc42916466952feb4f7e8ff2b24
Naman Khattar <khattarnaman915@gmail.com>
2022-10-16 12:32:17 +0530

    Minor Changes

--- ib64.py
+++ ib64.py
@@ -4,7 +4,7 @@ import base64
 
 def image_to_base64(image):
     buffered = BytesIO()
-    image.save(buffered, format="JPEG")
     image.convert("RGB");
+    image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue())
     return img_str
